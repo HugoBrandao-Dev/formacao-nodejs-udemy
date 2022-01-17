9fc883fd82e729cbb1cab1246e54055222fb845c
HugoBrandao <hugobrandao.dev@gmail.com>
2022-01-17 11:54:41 -0300

    Adicionado primeiro script do curso, contendo variaveis, consoles e uma condicional.

---
+++ 01-introducao-ao-nodejs/01-seu-primeiro-codigo-com-nodejs/aula001.js
@@ -0,0 +1,10 @@
+var mostrarSite = true
+const site = 'www.guiadoprogramador.com'
+
+console.log("Hello World!")
+console.log("Meu nome é Hugo")
+console.log("E eu estou aprendendo Node.JS com o Guia do Programador")
+
+if (mostrarSite) {
+	console.log(site)
+}
