0be739ef3f8a35d7c9773e737d7a0e85535daf75
HugoBrandao <hugobrandao.dev@gmail.com>
2022-01-18 12:35:23 -0300

    Modificado o arquivo indexjs, para que pudessemos passar parametros via url e fazer suas capturas.

--- 03-fundamento-do-expressjs/01-o-que-e-o-expressjs-e-npm/index.js
+++ 03-fundamento-do-expressjs/01-o-que-e-o-expressjs-e-npm/index.js
@@ -11,15 +11,29 @@ app.get("/", function(requisicao, resposta) {
 	Resposta para as requisicoes feitas na rota "/", só pode ser enviada uma única
 	vez (não deve haver mais de um .send)
 	*/
-	resposta.send("Bem vindo ao Guia do Programador.")
+	resposta.send("<h1>Bem vindo ao Guia do Programador.</h1>")
 })
 
 app.get("/blog", function(requisicao, resposta) {
-	resposta.send("Bem vindo ao meu blog")
+	resposta.send("<h2>Bem vindo ao meu blog.</h2>")
 })
 
 app.get("/canal/youtube", function(requisicao, resposta) {
-	resposta.send("Bem vindo ao meu canal no youtube.")
+	resposta.send("<h3>Bem vindo ao meu canal no youtube.</h3>")
+})
+
+app.get("/usuario/:nome/:empresa/:cargo?", function(req, res) {
+	let nome = req.params.nome
+	let empresa = req.params.empresa
+	let cargo = req.params.cargo
+
+	let texto = ''
+	if (cargo) {
+		texto = `${ nome } trabalha na empresa ${ empresa } como ${ cargo }.`
+	} else {
+		texto = `${ nome } trabalha na empresa ${ empresa }.`
+	}
+	res.send(texto)
 })
 
 /*
