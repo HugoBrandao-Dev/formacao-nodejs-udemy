364c306f3c8131618ad1cd0b6dec2385ecf5907f
HugoBrandao <hugobrandao.dev@gmail.com>
2022-01-18 11:46:48 -0300

    Modificado o arquivo index.js, onde criamos as rotas para a aplicacao.

--- 03-fundamento-do-expressjs/01-o-que-e-o-expressjs-e-npm/index.js
+++ 03-fundamento-do-expressjs/01-o-que-e-o-expressjs-e-npm/index.js
@@ -4,6 +4,24 @@ const express = require("express")
 // Iniciamos o framework na constante app.
 const app = express()
 
+// Devemos informar para qual URL a rota aponta e O QUE ELA FAZ.
+app.get("/", function(requisicao, resposta) {
+
+	/*
+	Resposta para as requisicoes feitas na rota "/", só pode ser enviada uma única
+	vez (não deve haver mais de um .send)
+	*/
+	resposta.send("Bem vindo ao Guia do Programador.")
+})
+
+app.get("/blog", function(requisicao, resposta) {
+	resposta.send("Bem vindo ao meu blog")
+})
+
+app.get("/canal/youtube", function(requisicao, resposta) {
+	resposta.send("Bem vindo ao meu canal no youtube.")
+})
+
 /*
 listen recebe a porta que atenderá as requisições e uma funcao, que tem um
 parâmetro erro
