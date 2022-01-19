9bf395a2f8d662ba4296cc0b97c1c1e425b05c8f
HugoBrandao <hugobrandao.dev@gmail.com>
2022-01-19 18:03:39 -0300

    Limpezas dos arquivos (retiradas todas as explicacoes) para se trabalhar, de fato, com o projeto referente a secao. Tambem foi instalado o bootstrap 4.3.1 (copiando e colando as pastas css e o js).

--- projetos/01-criando-uma-plataforma-de-perguntas-e-respostas-com-mysql/index.js
+++ projetos/01-criando-uma-plataforma-de-perguntas-e-respostas-com-mysql/index.js
@@ -5,32 +5,8 @@ const app = express()
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 
-app.get("/:nome/:lang", (req, res) => {
-	let nome = req.params.nome
-	let lang = req.params.lang
-	let mostrarMensagem = true
-
-	let produtos = [
-		{
-			nome: "Cachorro Quente",
-			preco: 15
-		}, {
-			nome: "Hamburguer",
-			preco: 20
-		},
-		{
-			nome: "Pizza",
-			preco: 40
-		}
-	]
-
-	res.render("index", {
-		nome: nome,
-		lang: lang,
-		profissao: "Desenvolvedor Web",
-		mostrarMensagem: mostrarMensagem,
-		produtos: produtos
-	})
+app.get("/", (req, res) => {
+	res.render("index")
 })
 
 app.listen(4000, erro => {
