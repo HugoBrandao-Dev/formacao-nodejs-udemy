d0a14aba5610099cd83a64d54338ca1152eeda5d
HugoBrandao <hugobrandao.dev@gmail.com>
2022-01-19 12:17:17 -0300

    Aprendendo a como utilizar variaveis JS dentro do template.

--- projetos/01-criando-uma-plataforma-de-perguntas-e-respostas-com-mysql/index.js
+++ projetos/01-criando-uma-plataforma-de-perguntas-e-respostas-com-mysql/index.js
@@ -4,8 +4,14 @@ const app = express()
 // Informa que o view engine, com que o Express irá trabalhar, será o EJS.
 app.set("view engine", "ejs")
 
-app.get("/", (req, res) => {
-	res.render("index")
+app.get("/:nome/:lang", (req, res) => {
+	let nome = req.params.nome
+	let lang = req.params.lang
+	res.render("index", {
+		nome: nome,
+		lang: lang,
+		profissao: "Desenvolvedor Web"
+	})
 })
 
 app.listen(4000, erro => {
