f473138666f45c30b111c20e3540013f0e30c507
HugoBrandao <hugobrandao.dev@gmail.com>
2022-01-19 21:55:16 -0300

    Utilizamos uma biblioteca do express, chamada body-parser, para fazer a decodificacao dos dados enviados pelo formulario.

--- projetos/01-criando-uma-plataforma-de-perguntas-e-respostas-com-mysql/index.js
+++ projetos/01-criando-uma-plataforma-de-perguntas-e-respostas-com-mysql/index.js
@@ -1,16 +1,28 @@
 const express = require("express")
 const app = express()
+const bodyParser = require("body-parser")
 
 // Informa que o view engine, com que o Express irá trabalhar, será o EJS.
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 
+// Body-parser
+app.use(bodyParser.urlencoded({extended: false}))
+// app.use(bodyParser.json())
+
+// Rotas
 app.get("/", (req, res) => {
 	res.render("index")
 })
 
 app.get("/perguntar", (req, res) => {
 	res.render("perguntar")
+})
+
+app.post("/salvarPergunta", (req, res) => {
+	let titulo = req.body["ipt-titulo"]
+	let descricao = req.body["txt-descricao"]
+	res.send(`<strong>Título: </strong>${ titulo }<br><strong>Descrição: </strong>${ descricao }`)
 })
 
 app.listen(4000, erro => {
