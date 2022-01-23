43ab6999f4a269b3dfc075a933a9bf0ecfe40d91
HugoBrandao <hugobrandao.dev@gmail.com>
2022-01-23 16:10:56 -0300

    Adicionado parte backend do formulario de artigos, onde os mesmo poderao ser salvos no banco de dados.

--- projetos/02-criando-um-blog-com-painel-administrativo-crud-completo/articles/articleController.js
+++ projetos/02-criando-um-blog-com-painel-administrativo-crud-completo/articles/articleController.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const router = express.Router()
 const Category = require("../categories/category")
+const Article = require("./article")
+const sligify = require("slugify")
+const { default: slugify } = require("slugify")
 
 router.get("/articles", (req, res) => {
 	res.send("Rota para articles")
@@ -15,6 +18,18 @@ router.get("/admin/articles/new", (req, res) => {
 		.then((categories) => {
 			res.render("admin/articles/new", { categories: categories })
 		})
+})
+
+router.post("/articles/save", (req, res) => {
+	let title = req.body["ipt-title"]
+	let body = req.body["txt-body"]
+	let categoryId = req.body["slt-category"]
+	Article.create({
+		title: title,
+		slug: slugify(title),
+		body: body,
+		categoryId: categoryId
+	})
 })
 
 module.exports = router
