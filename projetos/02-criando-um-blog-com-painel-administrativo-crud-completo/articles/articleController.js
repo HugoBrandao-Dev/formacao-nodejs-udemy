434f0e629dda170818cffeacd3fcb73011bd0bf8
HugoBrandao <hugobrandao.dev@gmail.com>
2022-01-23 15:55:27 -0300

    Criado um dropdown para selecao da categoria pertencente ao artigo que usuario escrevera.

--- projetos/02-criando-um-blog-com-painel-administrativo-crud-completo/articles/articleController.js
+++ projetos/02-criando-um-blog-com-painel-administrativo-crud-completo/articles/articleController.js
@@ -1,12 +1,20 @@
 const express = require("express")
 const router = express.Router()
+const Category = require("../categories/category")
 
 router.get("/articles", (req, res) => {
 	res.send("Rota para articles")
 })
 
 router.get("/admin/articles/new", (req, res) => {
-	res.render("admin/articles/new")
+	/*
+	Passamos as categorias cadastradas no banco, para que 
+	possamos criar um dropdown delas.
+	*/
+	Category.findAll({})
+		.then((categories) => {
+			res.render("admin/articles/new", { categories: categories })
+		})
 })
 
 module.exports = router
