0b4e1ea129bf5772ed0c9e957b167128da4ba36c
HugoBrandao <hugobrandao.dev@gmail.com>
2022-01-24 23:20:23 -0300

    Adicionada a biblioteca bcrypt, usada na criacao de hash, para que possamos armazenar a senha do usuario de forma segura no banco de dados.

--- projetos/02-criando-um-blog-com-painel-administrativo-crud-completo/users/usersController.js
+++ projetos/02-criando-um-blog-com-painel-administrativo-crud-completo/users/usersController.js
@@ -2,6 +2,12 @@ const express = require("express")
 const router = express.Router()
 const User = require('./User')
 
+/*
+Biblioteca para a geração de Hash para senha que serão
+armazenadas no banco de dados.
+*/
+const bcrypt = require("bcryptjs")
+
 router.get("/admin/users", (req, res) => {
 	res.send("Listagem de usuários.")
 })
@@ -14,9 +20,20 @@ router.post("/users/create", (req, res) => {
 	let email = req.body["ipt-email"]
 	let password = req.body["ipt-password"]
 
-	res.json({
-		email,
-		password
+	// Número "tempero" para gerar a hash
+	let salt = bcrypt.genSaltSync(10)
+
+	// Gera uma hash
+	let hash = bcrypt.hashSync(password, salt)
+
+	User.create({
+		email: email,
+		password: hash
+	}).then(() => {
+		res.redirect("/")
+	})
+	.catch(error => {
+		res.redirect("/")
 	})
 })
 
