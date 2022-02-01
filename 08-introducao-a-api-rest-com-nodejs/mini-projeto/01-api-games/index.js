3a51def8481aab97fa2f1f6c8cf1137b260fa554
HugoBrandao <hugobrandao.dev@gmail.com>
2022-01-31 22:29:00 -0300

    Adicionada e configurada a rota que fara a busca por um jogo em especifico, baseado no id informado pelo usuario na url.

--- 08-introducao-a-api-rest-com-nodejs/mini-projeto/01-api-games/index.js
+++ 08-introducao-a-api-rest-com-nodejs/mini-projeto/01-api-games/index.js
@@ -29,11 +29,35 @@ let database = {
 	]
 }
 
+// Lista todos os jogos cadastrados
 app.get("/games", (req, res) => {
-	
+
 	// Define o código de estatus na resposta
 	res.statusCode = 200
 	res.json(database.games)
+})
+
+// Busca por um jogo baseado no id informado na requisição
+app.get("/game/:id", (req, res) => {
+	let id = parseInt(req.params.id)
+	if (id) {
+		if (!isNaN(id)) {
+			res.statusCode = 200
+
+			// Faz a busca por um jogo que corresponda ao ID informado
+			let found = database.games.find(game => game.id === id)
+
+			if (found) {
+				res.json(found)
+			} else {
+				res.sendStatus(404)
+			}
+		} else {
+			res.sendStatus(400)
+		}
+	} else {
+		res.sendStatus(400)
+	}
 })
 
 app.listen(4000, () => {
