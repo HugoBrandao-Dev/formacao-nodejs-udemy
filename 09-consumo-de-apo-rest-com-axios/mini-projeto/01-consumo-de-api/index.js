1b12d97cf5516689ccf8a5623362c53704bd402d
HugoBrandao <hugobrandao.dev@gmail.com>
2022-02-01 22:28:34 -0300

    Finalizado o mini projeto de consumo de API. Nesta ultima parte, foi adicionado o codigo que fara uma requisicao para atualizacao de um game.

--- 09-consumo-de-apo-rest-com-axios/mini-projeto/01-consumo-de-api/index.js
+++ 09-consumo-de-apo-rest-com-axios/mini-projeto/01-consumo-de-api/index.js
@@ -11,12 +11,12 @@ axios.get("http://localhost:4000/games")
 
 			// Cria uma nova string/tag para ser colocada dentro da tabela
 			let trItem = `<tr>
-			<th scope="row" data-id="${ game.id }">${game.id}</th>
+			<th scope="row" data-id="${ game.id }">${ game.id }</th>
 			<td data-title="${ game.title }">${ game.title }</td>
 			<td data-year="${ game.year }">${ game.year }</td>
 			<td data-price="${ game.price }">R$ ${ game.price }</td>
 			<td>
-				<a class="btn btn-warning mr-2">Editar</a>
+				<button class="btn btn-warning" onclick="editarGame(${ game.id })">Atualizar</button>
 				<button class="btn btn-danger" onclick="deletarGame(${ game.id })">Deletar</button>
 			</td>
 			</tr>`
@@ -32,7 +32,7 @@ let btnCadastrar = document.querySelector('button#btn-cadastrar')
 btnCadastrar.addEventListener('click', createGame)
 
 function createGame() {
-	let formGame = document.forms['form-criar-game']
+	let formGame = document.forms['form-game']
 	let iptTitle = formGame["ipt-title"]
 	let iptYear = formGame["ipt-year"]
 	let iptPrice = formGame["ipt-price"]
@@ -49,7 +49,7 @@ function createGame() {
 
 	axios.post('http://localhost:4000/game', gameObject)
 		.then(response => {
-			if (response.status == 200) {
+			if (response.status == 201) {
 				alert("Game cadastrado com sucesso.")
 			}
 		}).catch(error => {
@@ -66,4 +66,59 @@ function deletarGame(id) {
 				console.log(error)
 			})
 	}
+}
+
+let btnAtualizar = document.querySelector("button#btn-atualizar")
+btnAtualizar.addEventListener('click', atualizarGame)
+
+function editarGame(id) {
+	axios.get(`http://localhost:4000/game/${ id }`)
+		.then(response => {
+			let game = response.data
+
+			let formGame = document.forms['form-game']
+			let iptId = formGame['ipt-id']
+			let iptTitle = formGame['ipt-title']
+			let iptYear = formGame['ipt-year']
+			let iptPrice = formGame['ipt-price']
+
+			iptId.value = game.id
+			iptTitle.value = game.title
+			iptYear.value = game.year
+			iptPrice.value = game.price
+
+			btnAtualizar.removeAttribute('disabled')
+		}).catch(error => {
+			console.log(error)
+		})
+}
+
+function atualizarGame() {
+	let formGame = document.forms['form-game']
+	let iptId = formGame["ipt-id"]
+	let iptTitle = formGame["ipt-title"]
+	let iptYear = formGame["ipt-year"]
+	let iptPrice = formGame["ipt-price"]
+
+	let id = iptId.value
+	let title = iptTitle.value
+	let year = parseInt(iptYear.value)
+	let price = parseFloat(iptPrice.value).toFixed(2)
+
+	let gameObject = {
+		title,
+		year,
+		price
+	}
+
+	axios.put(`http://localhost:4000/game/${ id }`, gameObject)
+		.then(response => {
+			if (response.status == 200) {
+				btnAtualizar.setAttribute('disabled', 'true')
+				iptId.value = ''
+				alert("Game atualizado com sucesso.")
+			}
+		}).catch(error => {
+			console.log(error)
+		})
 }
