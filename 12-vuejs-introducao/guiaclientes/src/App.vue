8fa6ac6847b19c4ac29ab525fc5b9535acb9d9ea
HugoBrandao <hugobrandao.dev@gmail.com>
2022-02-10 23:59:06 -0300

    Adicionado codigo onde fizemos alguns exemplos de components e os importamos no template principal da aplicacao.

--- 12-vuejs-introducao/guiaclientes/src/App.vue
+++ 12-vuejs-introducao/guiaclientes/src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Guia clientes</h1>
+		<Cliente/>
+		<Produto/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cliente from './components/Cliente'
+import Produto from './components/Produto'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+	components: {
+		Cliente,
+		Produto
+	}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ 12-vuejs-introducao/guiaclientes/src/components/Cliente.vue
@@ -0,0 +1,25 @@
+<template>
+	<div id="cliente">
+		<h2 id="cliente-nome">Cliente aqui</h2>
+		<h3>Descrição cliente</h3>
+	</div>
+</template>
+
+<script>
+
+</script>
+
+<!--
+scope indica que o estilo em questão só será aplicado a este componente.
+-->
+<style scoped>
+	#cliente {
+		color: green;
+		background-color: black;
+		width: 600px;
+		height: 120px;
+	}
+	#cliente-nome {
+		color: white;
+	}
+</style>
