f6c4d15521d8dca8753a86d6a0262eecab558cc1
HugoBrandao <hugobrandao.dev@gmail.com>
2022-02-11 01:32:40 -0300

    Adicinado codigo da aula onde aprendemos a interpolar dados, para ver como a reatividade funciona na pratica.

--- 12-vuejs-introducao/guiaclientes/src/components/Cliente.vue
+++ 12-vuejs-introducao/guiaclientes/src/components/Cliente.vue
@@ -2,11 +2,26 @@
 	<div id="cliente">
 		<h2 id="cliente-nome">Cliente aqui</h2>
 		<h3>Descrição cliente</h3>
+		<!-- Posso colocar expressões JavaScript que retornem algum valor -->
+		<p>Nome: {{ nome }}</p>
+		<p>Idade: {{ idade }} ano(s)</p>
+		<p>Telefone: {{ telefone }}</p>
+		<p>Email: {{ email }}</p>
 	</div>
 </template>
 
 <script>
-
+export default {
+	// Habilita criar dados que podem ser usados dentro do template.
+	data() {
+		return {
+			nome: 'Tobias de Oliveira',
+			telefone: '(22) 22222-2222',
+			email: 'tobias@gmail.com',
+			idade: 21
+		}
+	}
+}
 </script>
 
 <!--
@@ -14,10 +29,10 @@ scope indica que o estilo em questão só será aplicado a este componente.
 -->
 <style scoped>
 	#cliente {
+		padding: 10px;
 		color: green;
 		background-color: black;
 		width: 600px;
-		height: 120px;
 	}
 	#cliente-nome {
 		color: white;
