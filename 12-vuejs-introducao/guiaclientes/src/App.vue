3bf16c761afbe67ea2d78954fcc4bebfadc297f4
HugoBrandao <hugobrandao.dev@gmail.com>
2022-02-11 18:05:51 -0300

    Feito codigo onde adicionamos uma classe a uma div, utilizando a diretiva v-if e o data binding.

--- 12-vuejs-introducao/guiaclientes/src/App.vue
+++ 12-vuejs-introducao/guiaclientes/src/App.vue
@@ -1,79 +1,30 @@
 <template>
   <div id="app">
-		<input type="text" name="iptDinora" v-model="cliente.nome">
-    <h1>Guia clientes</h1>
-		<Cliente class="cliente"
-			:cliente="cliente"
-			:mostrarIdade="true"
-		/>
-		<Cliente class="cliente"
-			:cliente="cliente"
-			:mostrarIdade="false"
-		/>
-		<Cliente class="cliente"
-			:cliente="cliente"
-			:mostrarIdade="true"
-		/>
-		<!-- <form class="form-tobias">
-			<h3>Info do Tobias</h3>
-			<input type="text" name="iptNome" v-model="nomeTobias">
-			<input type="email" name="iptEmail" v-model="emailTobias">
-			<input type="number" name="iptIdade" v-model="idadeTobias">
-		</form> -->
-		<!-- nome, email e idade são props passadas ao componente cliente -->
-		<!-- <Cliente class="cliente"
-			:nome="nomeTobias"
-			:email="emailTobias"
-			:idade="idadeTobias"
-		/>
-		<Cliente class="cliente"
-			nome="Josias Cruz"
-			email="josias_rs@gmail.com"
-			idade="34"
-		/>
-		<Cliente class="cliente"
-			nome="Doralice Cruz"
-			email="doralice_to@hotmail.com"
-			idade="41"
-		/> -->
-		<!-- <Produto/> -->
+		<h1>Guia Cliente</h1>
+		<Cliente :cliente="cliente" :mostrarIdade="true"/>
   </div>
 </template>
 
 <script>
 import Cliente from './components/Cliente'
-// import Produto from './components/Produto'
 
 export default {
   name: 'App',
 	data() {
 		return {
-			nomeTobias: 'Tobias de Oliveira',
-			emailTobias: 'tobias_123@gmail.com',
-			idadeTobias: 34,
 			cliente: {
-				nome: 'Dinorá de Oliveira',
-				idade: 49,
-				telefone: '(20) 55555-5555',
-				email: 'dinora@hotmail.com'
+				nome: 'Tobias de Oliveira',
+				idade: 32,
+				email: 'tobias32@gmail.com'
 			}
 		}
 	},
 	components: {
-		Cliente,
-		// Produto
+		Cliente
 	}
 }
 </script>
 
 <style>
-	#app .cliente {
-		margin-bottom: 10px;
-	}
-	.form-tobias {
-		display: flex;
-		flex-direction: column;
-		align-items: flex-start;
-		margin-bottom: 10px;
-	}
+
 </style>
--- 12-vuejs-introducao/guiaclientes/src/components/Cliente.vue
+++ 12-vuejs-introducao/guiaclientes/src/components/Cliente.vue
@@ -1,42 +1,21 @@
 <template>
-	<div id="cliente">
-		<h3>Informações do cliente</h3>
-		<div class="cliente-info">
-			<!-- <p><span>Nome</span>: {{ nome }}</p>
-			<p><span>Idade</span>: {{ idade }}</p>
-			<p><span>Telefone</span> : {{ telefone }}</p>
-			<p><span>Email</span>: {{ email }}</p>
-			<p><span>Descrição</span>: {{ descricao }}</p> -->
-			<p><span>Nome</span>: {{ cliente.nome }}</p>
-
-			<!-- Caso a condição seja falsA, o elemento SERÁ destruído. -->
-			<p v-if="mostrarIdade"><span>Idade</span>: {{ cliente.idade }}</p>
-			<!-- Também existe o v-else-if -->
-			<p v-else>A idade está escondida.</p>
-
-			<!-- Caso a condiçao seja falsa, o elemento NÃO SERÁ destruído. -->
-			<p v-show="!mostrarIdade"><span>Idade</span>: {{ cliente.idade }}</p>
-			<p><span>Telefone</span> : {{ cliente.telefone }}</p>
-			<p><span>Email</span>: {{ cliente.email }}</p>
-			<p><span>Descrição</span>: {{ descricao }}</p>
-		</div>
+	<div :class="{cliente: true, 'cliente-premium': isPremium, 'cliente-comum': !isPremium }">
+		<h2>{{ cliente.nome }}</h2>
+		<hr>
+		<p><span>Email:</span> {{ cliente.email }}</p>
+		<p v-if="mostrarIdade"><span>Idade: </span> {{ cliente.idade }} anos</p>
+		<p v-else>O usuário escondeu a idade.</p>
 	</div>
 </template>
 
 <script>
 export default {
-	// Habilita criar dados que podem ser usados dentro do template.
 	data() {
 		return {
-			telefone: '(22) 22222-2222',
-			descricao: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+			isPremium: false
 		}
 	},
-	// Atributos que serão recebidos via props
 	props: {
-		// nome: String,
-		// email: String,
-		// idade: Number,
 		cliente: Object,
 		mostrarIdade: Boolean
 	}
@@ -47,15 +26,18 @@ export default {
 scope indica que o estilo em questão só será aplicado a este componente.
 -->
 <style scoped>
-	#cliente {
-		padding: 10px;
-		background-color: #f2f2f2;
-		width: 600px;
+	.cliente {
+		width: 500px;
+		padding: 5px;
 	}
-	#cliente-nome {
-		color: white;
-	}
-	#cliente .cliente-info span {
+	.cliente span {
 		font-weight: bold;
+	}
+	.cliente-comum {
+		background-color: #f2f2f2;
+	}
+	.cliente-premium {
+		background-color: black;
+		color: yellow;
 	}
 </style>
