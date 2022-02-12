9a6255d1576b7a4d4d771e6979a5b267511823c3
HugoBrandao <hugobrandao.dev@gmail.com>
2022-02-12 16:05:21 -0300

    Configurada a API com o axios, onde e buscada uma lista de pokemons que serao mostrados no template.

--- projetos/04-pokedex-com-vuejs/src/App.vue
+++ projetos/04-pokedex-com-vuejs/src/App.vue
@@ -8,7 +8,29 @@
 import axios from 'axios'
 
 export default {
-  name: 'App'
+  name: 'App',
+	data() {
+		return {
+			pokemons: []
+		}
+	},
+
+	// A função created é uma função que é chamada na criação deste componente (App.vue). Ela existe em todos so componentes.
+	created: function() {
+
+		/*
+		API que retornará somente os pokémons da primeira geração, devido 
+		a uma limitação da própria API.
+		*/
+		axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+			.then(dados => {
+				this.pokemons = dados.data.results
+				console.log('Lista pega com sucesso.')
+			})
+			.catch(error => {
+				console.log(error)
+			})
+	}
 }
 </script>
 
