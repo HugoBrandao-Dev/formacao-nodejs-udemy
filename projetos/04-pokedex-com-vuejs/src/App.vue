b36e6d3b3618cf96687e7cd183ac5e63946103a6
HugoBrandao <hugobrandao.dev@gmail.com>
2022-02-12 22:03:25 -0300

    Adicionado sistema de busca e filtro de um pokemon, baseado no nome informado pelo usuario no campo de busca.

--- projetos/04-pokedex-com-vuejs/src/App.vue
+++ projetos/04-pokedex-com-vuejs/src/App.vue
@@ -2,9 +2,12 @@
   <div id="app">
 		<img src="./assets/logo.png" alt="Logo do Vue" title="Este é um projeto que utiliza o framework Vue JS.">
 		<hr>
-		<h1 class="is-size-1">Porkedex</h1>
+		<h1 class="is-size-1">Pokedex</h1>
 		<div class="column is-half is-offset-one-quarter">
-			<div class="pokemons" v-for="(pokemon, index) in pokemons" :key="index">
+			<input class="input is-success is-rounded is-small mb-2" type="text" placeholder="Buscar pokemon pelo nome" v-model="busca">
+			<button class="button is-rounded is-dark is-small is-outlined is-fullwidth" @click="buscar">Buscar</button>
+
+			<div class="pokemons" v-for="(pokemon, index) in filteredPokemons" :key="pokemon.url">
 				<Pokemon :name="pokemon.name" :url="pokemon.url" :num="index + 1" />
 			</div>
 		</div>
@@ -22,14 +25,16 @@ export default {
   name: 'App',
 	data() {
 		return {
-			pokemons: []
+			pokemons: [],
+			filteredPokemons: [],
+			busca: ''
 		}
 	},
 	components: {
 		Pokemon
 	},
 
-	// A função created é uma função que é chamada na criação deste componente (App.vue). Ela existe em todos so componentes.
+	// A função created é uma função que é chamada na criação deste componente (App.vue). Ela existe em todos os componentes.
 	created: function() {
 
 		/*
@@ -39,11 +44,35 @@ export default {
 		axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
 			.then(dados => {
 				this.pokemons = dados.data.results
+				this.filteredPokemons = dados.data.results
 				console.log('Lista de pokemons pega com sucesso.')
 			})
 			.catch(error => {
 				console.log(error)
 			})
+	},
+	/*
+	Para quando a busca era feita a medida que o usuário digitava o nome.
+	computed: {
+		resultadoBusca: function() {
+			if (this.busca) {
+				return this.pokemons.filter(pokemon => pokemon.name == this.busca)
+			} else {
+				return this.pokemons
+			}
+		}
+	},
+	*/
+	methods: {
+		buscar: function() {
+				if (this.busca) {
+					this.filteredPokemons = this.pokemons.filter(pokemon => {
+						return pokemon.name == this.busca
+					})
+				} else {
+					this.filteredPokemons = this.pokemons
+				}
+		}
 	}
 }
 </script>
