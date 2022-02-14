56922122d01eca7d4038b7138cff74c0cb70b956
HugoBrandao <hugobrandao.dev@gmail.com>
2022-02-14 18:00:01 -0300

    Criado sistema de validacao de dados para requisicoes feitas a rota User.

--- projetos/05-api-rest-de-usuarios-com-knexjs-e-jwt/controllers/UserController.js
+++ projetos/05-api-rest-de-usuarios-com-knexjs-e-jwt/controllers/UserController.js
@@ -2,8 +2,21 @@ class UserController {
 	async index(req, res) {}
 
 	async create(req, res) {
-		console.log(req.body)
-		res.send('Pegando o corpo da requisição!')
+		let { name, email, password } = req.body
+
+		if (!name || !email || !password) {
+			res.status(400)
+			if (!name) {
+				res.json({error: 'Nome inválido.'})
+			} else if (!email) {
+				res.json({error: 'Email inválido.'})
+			} else {
+				res.json({error: 'Senha inválida.'})
+			}
+		} else {
+			res.status(200)
+			res.send("Tudo OK!")
+		}
 	}
 }
 
