8bf3e847eed58ac477796cd4d94e751084f8cd47
Hugo da Rocha Brandão <91743423+HugoBrandao-Dev@users.noreply.github.com>
2022-02-16 20:51:10 -0300

    Update database.js

--- projetos/02-criando-um-blog-com-painel-administrativo-crud-completo/database/database.js
+++ projetos/02-criando-um-blog-com-painel-administrativo-crud-completo/database/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize")
 
 // Configurações da conexão com o banco de dados
-const connection = new Sequelize("guiapress", "root", "#Oguh591022#", {
+const connection = new Sequelize("guiapress", "root", "", {
 	host: "localhost",
 	dialect: "mysql",
 	// Para que o Sequelize registre, corretamente, o horário do Brasil. (UTC -3h)
