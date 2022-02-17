f24b31b6426f1fced73be18360d7c179ec857629
HugoBrandao <hugobrandao.dev@gmail.com>
2022-02-16 22:41:04 -0300

    Criado template que fara requisicoes de registros de novos usuarios.

---
+++ projetos/06-integracao-do-nodejs-com-o-vuejs/src/views/Register.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="divRegister">
+		<h1 class="title is-1">Registro de usuário</h1>
+		<hr>
+		<form>
+			<div class="columns is-mobile is-centered">
+				<div class="column is-half">
+					<label>Nome</label>
+					<input type="text" name="iptNome" class="input mb-3" placeholder="Informe seu nome">
+					<label>E-mail</label>
+					<input type="email" name="iptEmail" class="input mb-3" placeholder="exemplo@email.com">
+					<label>Senha</label>
+					<input type="password" name="iptPassword" class="input">
+					<hr>
+					<button type="button" class="button is-success">Cadastrar</button>
+				</div>
+			</div>
+		</form>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>
