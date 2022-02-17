5a73e4e855e5d5d598eda7264748cbae9e3e9662
HugoBrandao <hugobrandao.dev@gmail.com>
2022-02-17 00:48:22 -0300

    Capturamos o token gerado no login do usuario e o armazenamos dentro do localStorage do navegador.

--- projetos/06-integracao-do-nodejs-com-o-vuejs/src/views/Login.vue
+++ projetos/06-integracao-do-nodejs-com-o-vuejs/src/views/Login.vue
@@ -5,6 +5,9 @@
 			<div class="column is-half">
 				<hr>
 				<form>
+					<div class="notification is-danger" v-if="error">
+						{{ error }}
+					</div>
 					<label>Email:</label>
 					<input type="email" name="iptEmail" class="input" v-model="email" placeholder="Informe seu email">
 					<label>Senha:</label>
@@ -18,6 +21,8 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
 	data() {
 		return {
@@ -28,7 +33,18 @@ export default {
 	},
 	methods: {
 		login: function() {
-			console.log(this.email, this.password)
+			axios.post('http://localhost:4000/login', {
+				email: this.email,
+				password: this.password
+			}).then(res => {
+
+				// Guarda o token dentro do localStorage do navegador
+				localStorage.setItem("TokenAPIUser", res.data.token)
+				this.$router.push({name: 'Home'})
+			}).catch(error => {
+				let msgError = error.response.data.error
+				this.error = msgError
+			})
 		}
 	}
 }
