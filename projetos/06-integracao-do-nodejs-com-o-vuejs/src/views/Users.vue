f16a4125d3da7d9bff9cbdd396a848c3f1a3ebb2
HugoBrandao <hugobrandao.dev@gmail.com>
2022-02-17 16:25:02 -0300

    Listados no template do adminstrador as informacoes dos usuarios cadastrados no banco de dados

--- projetos/06-integracao-do-nodejs-com-o-vuejs/src/views/Users.vue
+++ projetos/06-integracao-do-nodejs-com-o-vuejs/src/views/Users.vue
@@ -1,12 +1,68 @@
 <template>
 	<div id="divUsers">
 		<h1 class="title is-1">Usuários</h1>
+		<div class="columns is-mobile is-centered">
+			<div class="column is-half">		
+				<table class="table">
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>Nome</th>
+							<th>Email</th>
+							<th>Cargo</th>
+							<th>Ações</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr v-for="user in users" :key="user.id">
+							<td>{{ user.id }}</td>
+							<td>{{ user.nome }}</td>
+							<td>{{ user.email }}</td>
+							<td>{{ user.role | formatRole }}</td>
+							<td>
+								<button class="button is-warning">Editar</button>
+								<button class="button is-danger ml-3">Deletar</button>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
-export default {
+import axios from 'axios'
 
+export default {
+	data() {
+		return {
+			users: null
+		}
+	},
+	created: function() {
+		let req = {
+			headers: {
+				'Authorization': `Bearer ${ localStorage.getItem('TokenAPIUser') }`
+			}
+		}
+
+		axios.get('http://localhost:4000/user', req)
+			.then(res => {
+				this.users = res.data
+			})
+			.catch(error => {
+				console.log(error)
+			})
+	},
+	filters: {
+		formatRole(value) {
+			if (value == 1) {
+				return "Administrador(a)"
+			}
+			return "Usuário(a)"
+		}
+	}
 }
 </script>
 
