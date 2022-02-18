ddf9d81022a5b342ae6e365fbeea4bf8380357eb
HugoBrandao <hugobrandao.dev@gmail.com>
2022-02-18 13:50:18 -0300

    Criada e configurada a estrutura inicial do projeto 07, onde criaremos um chat utilizando o Socket.IO.

---
+++ projetos/07-chat-com-socket-io/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+
+const http = require('http').createServer(app)
+
+const io = require('socket.io')(http)
+
+app.set('view engine', 'ejs')
+
+
+
+// Web Socket
+io.on('connection', socket => {
+	socket.on('disconnect', () => {
+		console.log(`${ socket.id } se desconectou.`)
+	})
+})
+
+// Rotas
+app.get('/', (req, res) => {
+	res.render('index')
+})
+
+http.listen(4000, () => {
+	console.log('SERVIDOR RODANDO...')
+})
