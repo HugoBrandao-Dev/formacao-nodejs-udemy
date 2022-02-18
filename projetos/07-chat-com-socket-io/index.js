5152fac38552eac4a72c784b4672156d1af37155
HugoBrandao <hugobrandao.dev@gmail.com>
2022-02-18 14:39:19 -0300

    Emitida mensagem de forma global, para que todos que estão conectados ao chat/aplicacao possam ver todas as mensagens enviadas.

--- projetos/07-chat-com-socket-io/index.js
+++ projetos/07-chat-com-socket-io/index.js
@@ -17,7 +17,20 @@ io.on('connection', socket => {
 	})
 
 	socket.on('msg', data => {
-		socket.emit('showmsg', data)
+
+		// O socket é somente o cliente que enviou a mensagem.
+		// socket.emit('showmsg', data)
+
+		
+		// Envia para todos que estão conectados, menos para quem enviou a mensagem.
+		// socket.broadcast.emit('showmsg', data)
+
+		/*
+		É o servidor, neste caso todos que estão conectados ao servidor 
+		verão mensagem enviada por um único usuário.
+		*/
+		io.emit('showmsg', data)
+		
 	})
 	
 })
