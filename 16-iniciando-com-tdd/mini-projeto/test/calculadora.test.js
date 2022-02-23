a540b85a2fb13dabbda6d5420f279e6eb11875d8
HugoBrandao <hugobrandao.dev@gmail.com>
2022-02-22 23:54:20 -0300

    Criados os primeiros testes, neste caso para operacoes basicas de uma calculadora.

---
+++ 16-iniciando-com-tdd/mini-projeto/test/calculadora.test.js
@@ -0,0 +1,32 @@
+const app = require('../app')
+
+/*
+Coloca-se dentro do describe, quando há um Suite de test muito grande,
+como em formulários, por exemplo.
+*/
+describe('Operações básicas', () => {
+
+	// [O QUE O TESTE FAZ], [PROGRAMAÇÃO]
+	test('Deve retornar o valor 10 ao somar 5 + 5.', () => {
+
+		let resultado = app.somar(5, 5)
+		expect(resultado).toEqual(10)
+
+	})
+
+	test('Deve retornar o valor 50 ao multiplicar 5 * 10.', () => {
+		let resultado = app.multiplicar(5, 10)
+		expect(resultado).toEqual(50)
+	})
+
+	test('Deve retornar o valor 5 ao dividir 25 / 5.', () => {
+		let resultado = app.dividir(25, 5)
+		expect(resultado).toEqual(5)
+	})
+
+	test('Deve returnar o valor 2 ao subtrair 5 - 3.', () => {
+		let resultado = app.subtrair(5, 3)
+		expect(resultado).toEqual(2)
+	})
+	
+})
