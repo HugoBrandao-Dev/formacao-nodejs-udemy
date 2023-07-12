d0b5d35cea26b1e435be080200d3a5ea77d0c7e1
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-07-12 19:12:05 -0400

    Implementado codigo de conexao com o MongoDB Atlas.

--- projetos/09-sistema-de-compartilhamento-de-imagens-com-tdd-e-mongodb/src/app.js
+++ projetos/09-sistema-de-compartilhamento-de-imagens-com-tdd-e-mongodb/src/app.js
@@ -1,8 +1,14 @@
 let express = require('express')
 let app = express()
+let mongoose = require('mongoose')
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+mongoose.connect('mongodb+srv://hugobrandaodev:OdkgGWltotsvcsrL@cluster0.kmycn36.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology:true
+})
 
 app.get('/', function(req, res) {
   res.json({})
