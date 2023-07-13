b2f1cbe4969deea2a2bf6f48e0e575234dab43e0
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-07-13 14:40:55 -0400

    Criado endpoint para cadastro de um novo usuario.

--- projetos/09-sistema-de-compartilhamento-de-imagens-com-tdd-e-mongodb/src/app.js
+++ projetos/09-sistema-de-compartilhamento-de-imagens-com-tdd-e-mongodb/src/app.js
@@ -14,15 +14,31 @@ mongoose.connect('mongodb+srv://hugobrandaodev:OdkgGWltotsvcsrL@cluster0.kmycn36
 
   })
   .catch(function(error) {
-    console.error(error)
+    console.log(error)
   })
 
+// Models
+let user = require('./models/User')
+let User = mongoose.model("User", user)
+
+// Rotas
 app.get('/', function(req, res) {
   res.json({})
 })
 
-app.post('/user', function(req, res) {
-  res.json({})
+app.post('/user', async function(req, res) {
+  try {
+    let newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      senha: req.body.password
+    })
+
+    await newUser.save()
+    res.json({email: req.body.email})
+  } catch (error) {
+    res.sendStatus(500)
+  }
 })
 
 module.exports = app
