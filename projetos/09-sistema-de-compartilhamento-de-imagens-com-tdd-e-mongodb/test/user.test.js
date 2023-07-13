bba3888c0d5c25844fd6cc2354dbedceb02a8e4a
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-07-13 19:01:53 -0400

    Criado teste que deve impedir o cadastramento de um usuario que nao informa os dados.

--- projetos/09-sistema-de-compartilhamento-de-imagens-com-tdd-e-mongodb/test/user.test.js
+++ projetos/09-sistema-de-compartilhamento-de-imagens-com-tdd-e-mongodb/test/user.test.js
@@ -21,4 +21,16 @@ describe("Deve cadastrar um usuário com sucesso", function() {
         fail(error)
       })
   })
+
+  test("Deve impedir que um usuário se cadastre com dados vazios", function() {
+    let user = { name: "", email: "", password: "" }
+
+    return request.post("/user").send(user)
+      .then(function(res) {
+        expect(res.statusCode).toEqual(400)
+      })
+      .catch(function(error) {
+        fail(error)
+      })
+  })
 })
