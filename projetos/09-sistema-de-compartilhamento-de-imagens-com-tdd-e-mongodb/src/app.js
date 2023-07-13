96792ecd4c8131473b7cc96d3bad38e8329286fb
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-07-13 14:16:12 -0400

    Configurado o Jest para uso com o Mongoose.

--- projetos/09-sistema-de-compartilhamento-de-imagens-com-tdd-e-mongodb/src/app.js
+++ projetos/09-sistema-de-compartilhamento-de-imagens-com-tdd-e-mongodb/src/app.js
@@ -5,12 +5,23 @@ let mongoose = require('mongoose')
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-mongoose.connect('mongodb+srv://hugobrandaodev:OdkgGWltotsvcsrL@cluster0.kmycn36.mongodb.net/?retryWrites=true&w=majority', {
+let connectionParams = {
   useNewUrlParser: true,
   useUnifiedTopology:true
-})
+}
+mongoose.connect('mongodb+srv://hugobrandaodev:OdkgGWltotsvcsrL@cluster0.kmycn36.mongodb.net/?retryWrites=true&w=majority', connectionParams)
+  .then(function(res) {
+
+  })
+  .catch(function(error) {
+    console.error(error)
+  })
 
 app.get('/', function(req, res) {
+  res.json({})
+})
+
+app.post('/user', function(req, res) {
   res.json({})
 })
 
