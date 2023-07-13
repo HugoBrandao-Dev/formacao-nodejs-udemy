08da31204db6e68d06afc1eff1c8cf9fc4d47dc4
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-07-13 19:44:12 -0400

    Criado teste que monitora o impedimento de cadastro de um usuario com e-mail ja cadastrado.

--- projetos/09-sistema-de-compartilhamento-de-imagens-com-tdd-e-mongodb/test/user.test.js
+++ projetos/09-sistema-de-compartilhamento-de-imagens-com-tdd-e-mongodb/test/user.test.js
@@ -34,6 +34,34 @@ describe("Deve cadastrar um usuário com sucesso", function() {
         fail(error)
       })
   })
+
+  test("Deve impedir que um usuário se cadastre com um e-mail repetido.", function() {
+    let time = Date.now()
+    let email = `${ time }@yahoo.com`
+    let user = {
+      name: "Tobias de Oliveira",
+      email,
+      password: 'tobias123'
+    }
+
+    return request.post("/user").send(user)
+      .then(function(res) {
+        expect(res.statusCode).toEqual(200)
+        expect(res.body.email).toBe(email)
+
+        return request.post("/user").send(user)
+          .then(function(res) {
+            expect(res.statusCode).toEqual(400)
+            expect(res.body.error).toBe('E-mail já cadastrado.')
+          })
+          .catch(function(error) {
+            fail(error)
+          })
+      })
+      .catch(function(error) {
+        fail(error)
+      })
+  })
 })
 
 afterAll(async function() {
