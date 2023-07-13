fc5aa65ff1db14d0237caa28ac89bb84aab143e5
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-07-13 12:24:01 -0400

    Criado arquivo para testes nas rotas do User. Criado teste para a insercao de um novo usuario.

---
+++ projetos/09-sistema-de-compartilhamento-de-imagens-com-tdd-e-mongodb/test/user.test.js
@@ -0,0 +1,24 @@
+let app = require('../src/app')
+let supertest = require('supertest')
+let request = supertest(app)
+
+describe("Deve cadastrar um usuário com sucesso", function() {
+  test("Deve cadastrar um usuário com sucesso.", function() {
+    let time = Date.now()
+    let email = `${ time }@yahoo.com`
+    let user = {
+      name: "Tobias de Oliveira",
+      email,
+      password: 'tobias123'
+    }
+
+    return request.post("/user").send(user)
+      .then(function(res) {
+        expect(res.statusCode).toEqual(200)
+        expect(res.body.email).toBe(email)
+      })
+      .catch(function(error) {
+        fail(error)
+      })
+  })
+})
